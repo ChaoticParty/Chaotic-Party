0773b91b030bd1a98be71682151588a2968d9127
Bastien <bastien@millionvictories.com>
2022-11-15 13:59:29 +0100

    Debut code tacle

---
+++ Chaotic Party/Assets/Scripts/Cheats/ObjectTP.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectTP : MonoBehaviour
+{
+    public Vector2 coordonées;
+    public KeyCode inputActivation;
+}
