521713933957ccd016605219dfe0326b8d381bbb
Bastien <bastien@millionvictories.com>
2022-11-15 13:36:17 +0100

    Reglage soucis MultiplayerManager

--- Chaotic Party/Assets/Scripts/Controller/MultiplayerManager.cs
+++ Chaotic Party/Assets/Scripts/Controller/MultiplayerManager.cs
@@ -10,10 +10,15 @@ public class MultiplayerManager : MonoBehaviour
     private void Awake()
     {
         miniGameManager ??= FindObjectOfType<MiniGameManager>();
+        InitMultiplayer();
+    }
+
+    public void InitMultiplayer()
+    {
         for (int i = 0; i < Hinput.gamepad.Count; i++)
         {
             Gamepad gamepad = Hinput.gamepad[i];
-
+            
             if (players.Count <= i)
             {
                 return;
@@ -24,7 +29,7 @@ public class MultiplayerManager : MonoBehaviour
                 gamepad.Enable();
                 player.gamepad = gamepad;
                 player.index = i;
-                if(miniGameManager) miniGameManager.RegisterPlayer(player);
+                if (miniGameManager) miniGameManager.RegisterPlayer(player);
             }
             else
             {
@@ -32,5 +37,19 @@ public class MultiplayerManager : MonoBehaviour
                 if(player) player.gameObject.SetActive(false);
             }
         }
+    }
+    
+    public sbyte GamepadCount()
+    {
+        sbyte count = 0;
+        for (int i = 0; i < Hinput.gamepad.Count; i++)
+        {
+            Gamepad gamepad = Hinput.gamepad[i];
+            if (gamepad.isConnected)
+            {
+                count++;
+            }
+        }
+        return count;
     }
 }
