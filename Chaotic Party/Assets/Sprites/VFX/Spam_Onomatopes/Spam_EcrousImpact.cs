e596ded443cd30273b2472cf5563d3f8988a1035
Psym200 <lucas.lagresle@gmail.com>
2023-06-06 14:20:28 +0200

    Fx explosion

---
+++ Chaotic Party/Assets/Sprites/VFX/Spam_Onomatopes/Spam_EcrousImpact.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class Spam_EcrousImpact : MonoBehaviour
+{
+    public ParticleSystem particleSystem;
+    public List<Material> impactMaterial;
+
+    [Button]
+    public void EcrousImpact()
+    {
+        particleSystem.Play();
+        int RandomImpact = Random.Range(0, impactMaterial.Count);
+        particleSystem.GetComponent<Renderer>().material = impactMaterial[RandomImpact];
+    }
+}
