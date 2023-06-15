b52d2446f902875e5e1d0c972c4c435356eadb41
Bastien <bastien@millionvictories.com>
2023-06-15 14:35:45 +0200

    Inte sonore ui + 123 nuit

--- Chaotic Party/Assets/Scripts/MiniGame/Managers/CerbereManager.cs
+++ Chaotic Party/Assets/Scripts/MiniGame/Managers/CerbereManager.cs
@@ -14,6 +14,7 @@ public class CerbereManager : SpamManager
     [SerializeField] [Tooltip("Gameobject cheap du cerbere")] private GameObject cheapCerbere;
     [SerializeField] [Tooltip("Gameobject du cerbere")] private GameObject trueCerbere;
     [Header("Setup")]
+    [SerializeField] private SoundManager soundManager;
     [SerializeField] private CanvasGroup Hud;
     public GameObject[] hudMegaphone;
     [SerializeField] private GameObject nuitObject;
@@ -85,6 +86,7 @@ public class CerbereManager : SpamManager
     private new void Start()
     {
         base.Start();
+        soundManager.PlaySelfSound(gameObject.GetComponent<AudioSource>(), true);
     }
 
     private IEnumerator CheapCerbereAnimLaunch() //Workaround pour avoir le cerbere éveillé dès le début
@@ -259,6 +261,8 @@ public class CerbereManager : SpamManager
         
         nuitObject.SetActive(true);
         nuitObject.GetComponent<Animator>().SetTrigger("Bigger");
+        soundManager.PlaySelfSound(nuitObject.GetComponent<AudioSource>());
+        soundManager.EventStop("CerbereDodo");
         
         foreach (var animator in cerbereAnimator)
         {
@@ -277,6 +281,7 @@ public class CerbereManager : SpamManager
         foreach (var lasers in laserPlaceHolder)
         {
             lasers.SetActive(false);
+            soundManager.StopSelfSound(lasers.GetComponent<AudioSource>());
         }
         foreach (var animEvent in cerbereAnimEvents)
         {
@@ -306,6 +311,8 @@ public class CerbereManager : SpamManager
         
         yield return new WaitForSeconds(0.33f);
         
+        soundManager.EventPlay("CerbereDodo");
+        
         timeBeforeWake = Random.Range(cerberRompicheRangeMin, cerberRompicheRangeMax + 1);
         timePassedBeforeWake = timeBeforeWake;
 
@@ -317,6 +324,7 @@ public class CerbereManager : SpamManager
         yield return new WaitForSeconds(1f);
         scoreDisplay[index].text = "0";
         players[index].DegatGaucheLaser();
+        soundManager.PlaySelfSound(laserPlaceHolder[index].GetComponent<AudioSource>(), true);
         players[index].PlayHitSound();
         walkDestination[index] = xStartValuePos[index];
         playerGetBackToStart.Invoke(players[index].transform.position, "Argument");
@@ -416,6 +424,7 @@ public class CerbereManager : SpamManager
 
     private IEnumerator EndMiniGameAnim()
     {
+        soundManager.EventStop("CerbereDodo");
         int[] tabRank = new int[4];
         foreach (var player in ranking)
         {
@@ -441,6 +450,7 @@ public class CerbereManager : SpamManager
             if (i >= players.Count || i.Equals(winnerIndex)) continue;
             laserPlaceHolder[i].SetActive(true);
             yield return new WaitForSeconds(1);
+            soundManager.PlaySelfSound(laserPlaceHolder[i].GetComponent<AudioSource>(), true);
             players[i].DegatGaucheLaser();
             players[i].PlayHitSound();
             players[i].ChangeBulleText("!");
@@ -451,6 +461,11 @@ public class CerbereManager : SpamManager
         players[winnerIndex].Releve();
         players[winnerIndex].VictoryAnimation();
         yield return new WaitForSeconds(4);
+        soundManager.StopSelfSound(gameObject.GetComponent<AudioSource>());
+        foreach (var laser in laserPlaceHolder)
+        {
+            soundManager.StopSelfSound(laser.GetComponent<AudioSource>());
+        }
         LoadRecap();
     }
 
